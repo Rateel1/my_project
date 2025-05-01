8ea48772d9aad0216e2dc7a5278ba69377527d2c
Rateel1 <kadya2@gmail.com>
2025-05-01 19:16:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -295,7 +295,7 @@ with col2:
 
         selected_district = st.selectbox("اختر الحي 🏙️", district_data, format_func=lambda x: f"{x[1]} ({x[2]})")
   
-        #district = selected_district[1]
+        district = selected_district[1]
         #city_id = city_name_to_id[selected_district[2]]
 
         # ✅ Submit button inside the form

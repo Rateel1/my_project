41f988e2345b25723dcfa7c86d0b9e4baf32507c
Rateel1 <kadya2@gmail.com>
2025-05-17 23:11:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ st.markdown(
     div[data-testid="stForm"] select,
     div[data-testid="stForm"] button,
     div[data-testid="stForm"] div[role="slider"] {
-        font-size: 1.4rem !important;
+        font-size: 1.6rem !important;
     }
     </style>
     """,

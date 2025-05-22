87b18d3fa92dce2c9f57afc1dfc158aa85d4be03
Rateel1 <kadya2@gmail.com>
2025-05-22 18:40:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,16 +140,23 @@ with col2:
             area = st.number_input("", 150.0, 600.0, 150.0)
 
         with col_b:
-            street_width = st.selectbox("عرض الشارع (متر) 🛣️", [10, 12, 15, 18, 20, 25])
-            age = st.selectbox("عمر العقار 🗓️", list(range(0, 6)))
-            street_direction = st.selectbox("نوع الواجهة 🧭 🛣️", [
+        
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>عرض الشارع (متر) 🛣️</label>", unsafe_allow_html=True)
+            street_width = st.selectbox("", [10, 12, 15, 18, 20, 25])
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>عمر العقار 🗓️</label>", unsafe_allow_html=True)
+            age = st.selectbox("", list(range(0, 6)))
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>نوع الواجهة 🧭 🛣️</label>", unsafe_allow_html=True)
+            street_direction = st.selectbox("", [
                 "واجهة شمالية", "واجهة شرقية", "واجهة غربية", "واجهة جنوبية",
                 "واجهة شمالية شرقية", "واجهة جنوبية شرقية", "واجهة جنوبية غربية", "واجهة شمالية غربية",
                 "الفلة تقع على ثلاثة شوارع", "الفلة تقع على أربعة شوارع"
             ])
-            ketchen = st.selectbox("المطبخ مجهز🍳؟", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
-            furnished = st.selectbox("الفلة مؤثثة 🪑؟", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>المطبخ مجهز🍳؟ </label>", unsafe_allow_html=True)
+            ketchen = st.selectbox("", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>الفلة مؤثثة 🪑؟</label>", unsafe_allow_html=True)
+            furnished = st.selectbox("", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
 
+           
         district = st.selectbox("اختر الحي 🏙️", district_centers['district'].unique().tolist(),
                                 index=district_centers['district'].tolist().index(st.session_state['selected_district']))
 

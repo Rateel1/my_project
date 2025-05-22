8bda70862f66e1ce7ecd57733a7120821ea837be
Rateel1 <kadya2@gmail.com>
2025-05-22 15:33:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,9 +191,11 @@ with col2:
                 "الفلة تقع على ثلاثة شوارع", "الفلة تقع على أربعة شوارع"
             ])
             st.markdown("<label style='font-size:1rem; font-weight:bold;'>المطبخ مجهز 🍳</label>", unsafe_allow_html=True)
-            ketchen = st.selectbox("[1,0],", format_func=lambda x: "نعم" if x == 1 else "لا")
+            ketchen = st.selectbox("", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
             st.markdown("<label style='font-size:1rem; font-weight:bold;'>الفلة مؤثثة 🪑؟</label>", unsafe_allow_html=True)
-            furnished = st.selectbox("[1,0],", format_func=lambda x: "نعم" if x == 1 else "لا")
+            furnished = st.selectbox("", [1, 0], format_func=lambda x: "نعم" if x == 1 else "لا")
+            st.markdown("<label style='font-size:1rem; font-weight:bold;'>المطبخ مجهز 🍳؟</label>", unsafe_allow_html=True)
+
 
         district = st.selectbox("اختر الحي 🏙️", district_centers['district'].unique().tolist(),
                                 index=district_centers['district'].tolist().index(st.session_state['selected_district']))

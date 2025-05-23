a78d41c0351be2b5b2e0e80a405ca431bbda2066
Rateel1 <kadya2@gmail.com>
2025-05-23 14:52:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ div[data-testid="stForm"] select,
 div[data-testid="stForm"] button,
 div[data-testid="stForm"] div[role="slider"] {
     font-size: 1.8rem !important;
+     width: 100%!important;
 }
 .stSelectbox div[data-baseweb="select"] > div {
     font-size: 1.6rem !important;
@@ -52,7 +53,7 @@ div[data-testid="stNumberInput"] label {
     font-size: 2rem !important;
     font-weight: bold !important;
     text-align: right;
-     width: 100%!important;
+     
 }
 
 </style>

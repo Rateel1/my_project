1f811fa95ba2ba3fa3a52ea7248315eae6e10b56
Rateel1 <kadya2@gmail.com>
2025-05-23 16:18:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ section[data-testid="stSidebar"] {
     text-align: right;
 }
 div[data-testid="stForm"] input,
-div[data-testid="stForm"] select,
+# div[data-testid="stForm"] select,
 div[data-testid="stForm"] button,
 div[data-testid="stForm"] div[role="slider"] {
     font-size: 1.8rem !important;
@@ -53,7 +53,7 @@ div[data-testid="stNumberInput"] label {
     font-size: 2rem !important;
     font-weight: bold !important;
     text-align: right;
-       width: 100%!important;
+      
 }
 
 </style>
